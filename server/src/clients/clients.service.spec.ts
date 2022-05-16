9a0b33da2225917325bb243b785a489639829760
Brenda <breeeendaqn@gmail.com>
2022-05-16 08:06:01 -0300

    test(clientsService): password hash compare in create() test

--- server/src/clients/clients.service.spec.ts
+++ server/src/clients/clients.service.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import * as bcrypt from 'bcrypt';
 import { ClientsRepository } from './clients.repository';
 import { ClientsService } from './clients.service';
 import { Client } from './entities/client.entity';
@@ -30,10 +31,32 @@ describe('ClientsService', () => {
         password: 'password',
       };
 
-      const response = await repository.save(newClient);
+      const response = await service.create(newClient);
 
-      jest.spyOn(repository, 'save').mockImplementation(() => newClient);
+      const passwordEquals = await bcrypt.compare(newClient.password, response.password);
+
+      if(passwordEquals){
+        response.password = newClient.password;
+      } 
+
       expect(response).toEqual(newClient);
     });
+
+    it('should throw an error if client not saved', async () => {
+      const newClient: Client = {
+        id: '000-abf31-432124',
+        age: 30,
+        documentNumber: '33311122208',
+        name: 'New Client',
+        password: 'password',
+      };
+  
+      jest.spyOn(service, 'create').mockRejectedValue(() => new Error('Error'));
+      
+      const response = await service.create(newClient);
+  
+      expect(response).rejects.toThrowError();
+    })
   });
+
 });
