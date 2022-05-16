a2770aaa582ca9c09a1af11937f392d1a50a8f77
Brenda <breeeendaqn@gmail.com>
2022-05-16 20:18:26 -0300

    feature: create new doctor endpoints

---
+++ server/src/doctors/doctors.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { DoctorsService } from './doctors.service';
+import { CreateDoctorDto } from './dto/create-doctor.dto';
+
+@Controller('doctors')
+export class DoctorsController {
+  constructor(private readonly doctorsService: DoctorsService) {}
+
+  @Post()
+  create(@Body() doctor: CreateDoctorDto) {
+    return this.doctorsService.create(doctor);
+  }
+}
