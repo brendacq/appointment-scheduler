e547afd2fb15124810eb6cce494700ee09fc9c1c
Brenda <breeeendaqn@gmail.com>
2022-05-16 21:40:39 -0300

    add providers

--- server/src/appointments/appointments.module.ts
+++ server/src/appointments/appointments.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppointmentsService } from './appointments.service';
 import { AppointmentsController } from './appointments.controller';
+import { AppointmentsRepository } from './appointments.repository';
 
 @Module({
   controllers: [AppointmentsController],
-  providers: [AppointmentsService]
+  providers: [AppointmentsService, AppointmentsRepository]
 })
 export class AppointmentsModule {}
