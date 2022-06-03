d47dfbb4dc224719c75b97c5d4da68d96b9236a8
Brenda <breeeendaqn@gmail.com>
2022-06-03 17:19:41 -0300

    refactor: add fields to appointments

--- server/src/appointments/entities/appointment.entity.ts
+++ server/src/appointments/entities/appointment.entity.ts
@@ -1,16 +1,36 @@
-import { Patient } from "src/patients/entities/patient.entity";
-import { Doctor } from "src/doctors/entities/doctor.entity";
+import { Patient } from "../../patients/entities/patient.entity";
+import { Doctor } from "../../doctors/entities/doctor.entity";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class Appointment {
+    @ApiProperty()
     id: string; 
 
+    @ApiProperty()
     patient: Patient;
 
+    @ApiProperty()
     doctor: Doctor;
 
+    @ApiProperty()
     appointmentDate: Date;
 
+    @ApiProperty()
+    value: number;
+
+    @ApiProperty()
+    paymentMethod: string;
+
+    @ApiProperty()
+    status: string;
+
+    @ApiProperty()
     createdAt: string;
 
+    @ApiProperty()
     updatedAt: string;
+}
+
+export class Appointments {
+    appointments: Appointment[];
 }
